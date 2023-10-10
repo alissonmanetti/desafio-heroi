7ee9c713ab9c38488943ffa49c202ce3fd396fe4
Alisson Lemos Manetti <alisson.manetti@hotmail.com>
2023-10-10 04:45:38 -0300

    Desafio Herói

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("oi")
+let nomeHeroi = "Multeck";
+let experienciaHeroi = 1001;
+
+if (experienciaHeroi <= 1000) {
+  experienciaHeroi = "Ferro";
+} else if (experienciaHeroi >= 1001 && experienciaHeroi <= 2000) {
+  experienciaHeroi = "Bronze";
+} else if (experienciaHeroi >= 2001 && experienciaHeroi <= 5000) {
+  experienciaHeroi = "Prata";
+} else if (experienciaHeroi >= 6001 && experienciaHeroi <= 7000) {
+  experienciaHeroi = "Ouro";
+} else if (experienciaHeroi >= 7001 && experienciaHeroi <= 8000) {
+  experienciaHeroi = "Platina";
+} else if (experienciaHeroi >= 8001 && experienciaHeroi <= 9000) {
+  experienciaHeroi = "Ascendente";
+} else if (experienciaHeroi >= 9001 && experienciaHeroi <= 10000) {
+  experienciaHeroi = "Imortal";
+} else if (experienciaHeroi >= 10001) {
+  experienciaHeroi = "Radiante";
+}
+
+console.log(
+  `O Herói de nome ${nomeHeroi} está no nivel de ${experienciaHeroi}`
+);
